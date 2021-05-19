63e4a57b19c8a18a0f5671cf2aed756446ccc566
Alexandr <Almixleon@yandex.ru>
2021-05-19 22:38:35 +0400

    Дополнил дизайн приложения

--- app/src/main/java/com/example/weatherapp/AppState.kt
+++
@@ -1,8 +0,0 @@
-package com.example.weatherapp
-
-sealed class AppState {
-    data class Success(val weatherData: Any) : AppState()
-    data class Error(val error: Throwable) : AppState()
-    object Loading : AppState()
-
-}
