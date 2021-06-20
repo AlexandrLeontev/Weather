db76d0a00ad0461385b61cf8d394d07a2937496b
Alexandr <Almixleon@yandex.ru>
2021-06-20 17:49:31 +0400

    Убрал баги

--- app/src/main/java/com/example/weatherapp/view/main/MainFragment.kt
+++ app/src/main/java/com/example/weatherapp/view/main/MainFragment.kt
@@ -14,6 +14,7 @@ import android.view.ViewGroup
 import androidx.appcompat.app.AlertDialog
 import androidx.core.content.ContextCompat
 import androidx.fragment.app.Fragment
+import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
 import com.example.weatherapp.R
 import com.example.weatherapp.app.AppState
@@ -23,9 +24,11 @@ import com.example.weatherapp.model.Weather
 import com.example.weatherapp.utils.showSnackBar
 import com.example.weatherapp.view.details.DetailsFragment
 import com.example.weatherapp.viewmodel.MainViewModel
+import com.google.android.material.snackbar.Snackbar
 import java.io.IOException
+import java.util.*
 
-private const val IS_WORLD_KEY = "LIST_OF_TOWNS_KEY" //Добавим ключ-константу
+private const val IS_WORLD_KEY = "LIST_OF_CITIES_KEY"
 private const val REQUEST_CODE = 12345
 private const val REFRESH_PERIOD = 60000L
 private const val MINIMAL_DISTANCE = 100f
@@ -34,8 +37,11 @@ class MainFragment : Fragment() {
 
     private var _binding: FragmentMainBinding? = null
     private val binding get() = _binding!!
-    private val viewModel: MainViewModel by lazy { ViewModelProvider(this).get(MainViewModel::class.java) }
-    private var isDataSetRus: Boolean = true
+
+    private val viewModel: MainViewModel by lazy {
+        ViewModelProvider(this).get(MainViewModel::class.java)
+    }
+    private var isDataSetWorld: Boolean = false
     private val adapter = MainFragmentAdapter(object : OnItemViewClickListener {
         override fun onItemViewClick(weather: Weather) {
             openDetailsFragment(weather)
@@ -43,53 +49,44 @@ class MainFragment : Fragment() {
     })
 
     private val onLocationListener = object : LocationListener {
-
         override fun onLocationChanged(location: Location) {
             context?.let {
                 getAddressAsync(it, location)
             }
         }
 
-        override fun onStatusChanged(provider: String, status: Int, extras: Bundle) {}
+        override fun onStatusChanged(provider: String?, status: Int, extras: Bundle?) {}
         override fun onProviderEnabled(provider: String) {}
         override fun onProviderDisabled(provider: String) {}
     }
 
     override fun onCreateView(
-            inflater: LayoutInflater, container: ViewGroup?,
+            inflater: LayoutInflater,
+            container: ViewGroup?,
             savedInstanceState: Bundle?
     ): View {
         _binding = FragmentMainBinding.inflate(inflater, container, false)
         return binding.root
     }
 
-    //Получаем значения настроек через SharedPreferences и проверяем, есть ли там нужные нам данные
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         binding.mainFragmentRecyclerView.adapter = adapter
-        binding.mainFragmentFABLocation.setOnClickListener { checkPermission()}
         binding.mainFragmentFAB.setOnClickListener { changeWeatherDataSet() }
-
-        viewModel.getLiveData().observe(viewLifecycleOwner, { renderData(it) })
-
-        showListOfTowns()
-    }
-
-    override fun onDestroy() {
-        adapter.removeListener()
-        super.onDestroy()
+        viewModel.getLiveData().observe(viewLifecycleOwner, Observer { renderData(it) })
+        binding.mainFragmentFABLocation.setOnClickListener { checkPermission() }
+        showListOfCities()
     }
 
     override fun onRequestPermissionsResult(
             requestCode: Int,
-            permissions: Array<String>, grantResults: IntArray
+            permissions: Array<String>,
+            grantResults: IntArray
     ) {
         checkPermissionsResult(requestCode, grantResults)
     }
 
-    //Здесь мы получаем у контекста SharedPreferences, запрашиваем значение по ключу.
-    // Если такого значения там нет, то возвращаем false по умолчанию — так произойдёт при первом запуске приложения
-    private fun showListOfTowns() {
+    private fun showListOfCities() {
         activity?.let {
             if (it.getPreferences(Context.MODE_PRIVATE).getBoolean(IS_WORLD_KEY, false)) {
                 changeWeatherDataSet()
@@ -99,24 +96,23 @@ class MainFragment : Fragment() {
         }
     }
 
-    //Сохраним настройки в зависимости от выбора пользователя
     private fun changeWeatherDataSet() {
-        if (isDataSetRus) {
-            viewModel.getWeatherFromLocalSourceWorld()
-            binding.mainFragmentFAB.setImageResource(R.drawable.ic_earth)
-        } else {
+        if (isDataSetWorld) {
             viewModel.getWeatherFromLocalSourceRus()
             binding.mainFragmentFAB.setImageResource(R.drawable.ic_russia)
+        } else {
+            viewModel.getWeatherFromLocalSourceWorld()
+            binding.mainFragmentFAB.setImageResource(R.drawable.ic_earth)
         }
-        isDataSetRus = !isDataSetRus
+        isDataSetWorld = !isDataSetWorld
 
-        saveListOfTowns(isDataSetRus)
-    } //Теперь наше приложение запоминает, какой список городов был открыт в последний раз
+        saveListOfCities(isDataSetWorld)
+    }
 
-    private fun saveListOfTowns(isDataSetRus: Boolean) {
+    private fun saveListOfCities(isDataSetWorld: Boolean) {
         activity?.let {
             with(it.getPreferences(Context.MODE_PRIVATE).edit()) {
-                putBoolean(IS_WORLD_KEY, !isDataSetRus)
+                putBoolean(IS_WORLD_KEY, isDataSetWorld)
                 apply()
             }
         }
@@ -133,22 +129,22 @@ class MainFragment : Fragment() {
             }
             is AppState.Error -> {
                 binding.includedLoadingLayout.loadingLayout.visibility = View.GONE
-                binding.mainFragmentRootView.showSnackBar(
+                binding.mainFragmentRootView.showSnackbar(
                         getString(R.string.error),
                         getString(R.string.reload),
                         { viewModel.getWeatherFromLocalSourceRus() })
             }
         }
     }
+
     private fun checkPermission() {
         activity?.let {
             when {
-                ContextCompat.checkSelfPermission(it, Manifest.permission.ACCESS_FINE_LOCATION) ==
-                        PackageManager.PERMISSION_GRANTED -> {
+                ContextCompat.checkSelfPermission(it, Manifest.permission.ACCESS_FINE_LOCATION)
+                        == PackageManager.PERMISSION_GRANTED -> {
                     getLocation()
                 }
                 shouldShowRequestPermissionRationale(Manifest.permission.ACCESS_FINE_LOCATION) -> {
-
                     showRationaleDialog()
                 }
                 else -> {
@@ -199,12 +195,10 @@ class MainFragment : Fragment() {
             if (ContextCompat.checkSelfPermission(
                             context,
                             Manifest.permission.ACCESS_FINE_LOCATION
-                    ) ==
-                    PackageManager.PERMISSION_GRANTED
+                    ) == PackageManager.PERMISSION_GRANTED
             ) {
-                val locationManager =
-                        context.getSystemService(Context.LOCATION_SERVICE) as LocationManager
-
+                val locationManager = context.getSystemService(Context.LOCATION_SERVICE) as
+                        LocationManager
                 if (locationManager.isProviderEnabled(LocationManager.GPS_PROVIDER)) {
                     val provider = locationManager.getProvider(LocationManager.GPS_PROVIDER)
                     provider?.let {
@@ -237,10 +231,7 @@ class MainFragment : Fragment() {
         }
     }
 
-    private fun getAddressAsync(
-            context: Context,
-            location: Location
-    ) {
+    private fun getAddressAsync(context: Context, location: Location) {
         val geoCoder = Geocoder(context)
         Thread {
             try {
@@ -249,30 +240,22 @@ class MainFragment : Fragment() {
                         location.longitude,
                         1
                 )
-                if (addresses.isNotEmpty()){
-                    showDialog(String(), "Нет адреса")
-                    binding.mainFragmentFAB.post {
-                        showAddressDialog(addresses[0].getAddressLine(0), location)
-                    }
-
+                binding.mainFragmentFAB.post {
+                    showAddressDialog(addresses[0].getAddressLine(0), location)
                 }
-
             } catch (e: IOException) {
                 e.printStackTrace()
             }
         }.start()
     }
 
-    private fun openDetailsFragment(
-            weather: Weather
-    ) {
+    private fun openDetailsFragment(weather: Weather) {
         activity?.supportFragmentManager?.apply {
             beginTransaction()
                     .add(
-                            R.id.container,
-                            DetailsFragment.newInstance(Bundle().apply {
-                                putParcelable(DetailsFragment.BUNDLE_EXTRA, weather)
-                            })
+                            R.id.container, DetailsFragment.newInstance(Bundle().apply {
+                        putParcelable(DetailsFragment.BUNDLE_EXTRA, weather)
+                    })
                     )
                     .addToBackStack("")
                     .commitAllowingStateLoss()
@@ -283,11 +266,13 @@ class MainFragment : Fragment() {
         activity?.let {
             AlertDialog.Builder(it)
                     .setTitle(getString(R.string.dialog_rationale_title))
-                    .setMessage(getString(R.string.dialog_rationale_meaasge))
+                    .setMessage(getString(R.string.dialog_rationale_message))
                     .setPositiveButton(getString(R.string.dialog_rationale_give_access)) { _, _ ->
                         requestPermission()
                     }
-                    .setNegativeButton(getString(R.string.dialog_rationale_decline)) { dialog, _ -> dialog.dismiss() }
+                    .setNegativeButton(getString(R.string.dialog_rationale_decline)) { dialog, _ ->
+                        dialog.dismiss()
+                    }
                     .create()
                     .show()
         }
@@ -309,7 +294,9 @@ class MainFragment : Fragment() {
                                 )
                         )
                     }
-                    .setNegativeButton(getString(R.string.dialog_button_close)) { dialog, _ -> dialog.dismiss() }
+                    .setNegativeButton(getString(R.string.dialog_button_close)) { dialog, _ ->
+                        dialog.dismiss()
+                    }
                     .create()
                     .show()
         }
@@ -320,14 +307,21 @@ class MainFragment : Fragment() {
             AlertDialog.Builder(it)
                     .setTitle(title)
                     .setMessage(message)
-                    .setNegativeButton(getString(R.string.dialog_button_close)) { dialog, _ -> dialog.dismiss() }
+                    .setNegativeButton(getString(R.string.dialog_button_close)) { dialog, _ ->
+                        dialog.dismiss()
+                    }
                     .create()
                     .show()
         }
     }
 
-    interface OnItemViewClickListener {
-        fun onItemViewClick(weather: Weather)
+    override fun onDestroy() {
+        adapter.removeListener()
+        super.onDestroy()
+    }
+
+    companion object {
+        fun newInstance() = MainFragment()
     }
 
     override fun onDestroyView() {
@@ -335,7 +329,17 @@ class MainFragment : Fragment() {
         _binding = null
     }
 
-    companion object {
-        fun newInstance() = MainFragment()
+    interface OnItemViewClickListener {
+        fun onItemViewClick(weather: Weather)
     }
 }
+
+private fun View.showSnackbar(
+        text: String,
+        actionText: String,
+        action: (View) -> Unit,
+        length: Int = Snackbar.LENGTH_INDEFINITE
+) {
+    Snackbar.make(this, text, length).setAction(actionText, action).show()
+}
+
